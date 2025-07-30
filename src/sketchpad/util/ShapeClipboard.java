3e625d07bd69a1a024b913c0e7b66db23ca3115e
jiang <jiangfz1997@gmail.com>
2025-07-30 00:00:39 -0400

    feat: add pasteCount to adjust offset for multi paste

--- src/sketchpad/util/ShapeClipboard.java
+++ src/sketchpad/util/ShapeClipboard.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 public class ShapeClipboard {
     private static final List<Shape> clipboard = new ArrayList<>();
-
+    private static int pasteCount = 0;
     public static void copyFrom(List<Shape> shapes) {
         clipboard.clear();
+        pasteCount = 0;
         for (Shape s : shapes) {
             if (s.isSelected())
                 clipboard.add(s.clone());
@@ -18,9 +19,10 @@ public class ShapeClipboard {
 
     public static List<Shape> paste() {
         List<Shape> clones = new ArrayList<>();
+        pasteCount += 1;
         for (Shape s : clipboard) {
             Shape copy = s.clone();
-            copy.moveBy(10, 10);  // 轻微偏移
+            copy.moveBy(10*pasteCount, 10*pasteCount);  // 轻微偏移
             copy.setSelected(true);
             clones.add(copy);
         }
